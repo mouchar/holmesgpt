796eb9fbce336d318395a7979bb6a2218ac1cd67
Qingchuan Hao <qingchuan.hao@microsoft.com>
2026-03-05 03:32:49 +0000

    remove workaround for AZURE_API_VERSION and add UT
    Signed-off-by: Qingchuan Hao <qingchuan.hao@microsoft.com>

--- holmes/core/llm.py
+++ holmes/core/llm.py
@@ -253,19 +253,6 @@ class DefaultLLM(LLM):
             model_requirements = litellm.validate_environment(
                 model=model, api_key=api_key, api_base=api_base
             )
-            # validate_environment does not accept api_version, and as a special case for Azure OpenAI Service,
-            # when all the other AZURE environments are set expect AZURE_API_VERSION, validate_environment complains
-            # the missing of it even after the api_version is set.
-            # TODO: There's an open PR in litellm to accept api_version in validate_environment, we can leverage this
-            # change if accepted to ignore the following check.
-            # https://github.com/BerriAI/litellm/pull/13808
-            if (
-                provider == "azure"
-                and ["AZURE_API_VERSION"] == model_requirements["missing_keys"]
-                and api_version is not None
-            ):
-                model_requirements["missing_keys"] = []
-                model_requirements["keys_in_environment"] = True
 
         if not model_requirements["keys_in_environment"]:
             raise Exception(
